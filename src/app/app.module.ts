84bafcc26c502568e6ecf7d769fd31330b0c1696
remy.begue <remi.begue@atos.net>
2020-05-25 15:32:45 +0200

    generate modules and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { IconsModule } from './icons/icons.module';
+import { LoginModule } from './login/login.module';
+import { TextModule } from './text/text.module';
+import { UiModule } from './ui/ui.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule,
+    LoginModule,
+    TextModule,
+    IconsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
