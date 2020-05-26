dcbe03cda9ed728ccdc964b3d1b43547f11608f3
remy.begue <remi.begue@atos.net>
2020-05-26 11:08:28 +0200

    fontawesome and attr directive

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDeleteComponent } from './components/icon-delete/icon-delete.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IconNavComponent, IconEditComponent, IconDeleteComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [IconNavComponent, IconEditComponent, IconDeleteComponent]
 })
 export class IconsModule { }
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiComponent } from './components/ui/ui.component';
+import { IconsModule } from '../icons/icons.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UiComponent],
+  exports: [UiComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IconsModule
   ]
 })
 export class UiModule { }
