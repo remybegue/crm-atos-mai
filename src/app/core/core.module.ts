df10b5eae643faf32ba4c2c0b747afc1450aba6a
remy.begue <remi.begue@atos.net>
2020-05-26 12:16:46 +0200

    router part 1

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavComponent } from './components/nav/nav.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, NavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [HeaderComponent, FooterComponent, NavComponent]
 })
