ec83cb765f5e1ca365d0f8db58c188e6d0dcfa50
remy.begue <remi.begue@atos.net>
2020-05-26 11:36:31 +0200

    transclusion with ng-content

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent, NavComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HeaderComponent, FooterComponent, NavComponent]
 })
 export class CoreModule { }
