2497e5fb1687acd90a9ce5cc9cd002bc4deceb72
remy.begue <remi.begue@atos.net>
2020-05-28 16:26:24 +0200

    validators

--- src/app/orders/components/form-add-order/form-add-order.component.ts
+++ src/app/orders/components/form-add-order/form-add-order.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StateOrder } from 'src/app/shared/enums/state-order.enum';
 import { Order } from 'src/app/shared/models/order';
 
@@ -21,14 +21,17 @@ export class FormAddOrderComponent implements OnInit {
       nbJours: [this.item.nbJours],
       tva: [this.item.tva],
       state: [this.item.state],
-      typePresta: [this.item.typePresta],
-      client: [this.item.client],
+      typePresta: [this.item.typePresta, Validators.required],
+      client: [
+        this.item.client,
+        Validators.compose([Validators.required, Validators.minLength(2)]),
+      ],
       comment: [this.item.comment],
       id: [this.item.id],
     });
   }
 
-  public onSubmit(){
+  public onSubmit() {
     this.clicked.emit(this.form.value);
   }
 }
